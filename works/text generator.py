1b151698b17cb2f0c6bad6d31a27c599c018a7e0
ЛицейТПУ <114549805+lyctpu@users.noreply.github.com>
2022-11-27 23:10:17 +0700

    Add files via upload

---
+++ works/text generator.py
@@ -0,0 +1,48 @@
+def g122a():
+
+    t1='''
+    https://github.com/grad154/timurbakeev154
+    https://github.com/Dimakek2/work
+    https://github.com/weissok/-22
+    https://github.com/VoroninVaycheslav/LearnOfInvormatic
+    https://github.com/NikitaDavydov11/FirstRepository
+    https://github.com/AndreDem135/FirstRepository
+    https://github.com/Prostochell-228/UltraloxIT
+    https://github.com/schukchu/figushkiFiguli
+    https://github.com/evgrfg/123456
+    https://github.com/Aakookie/Kot
+    https://github.com/kirmala/school
+    https://github.com/pxnandi/tpu-learn
+    https://github.com/NightSkymbry/tpu-lic-Osipov-Slava'''
+
+    t=t1.split()
+    # print(t)
+    for i in t:
+        print(f'''var http = "{i}";
+    window.open(http);
+    ''')
+def g122b():
+    t1='''
+ https://github.com/churka1488/zzzzzzz/tree/main
+ https://github.com/sosiska256/Pirogov-Egor-Maksimovich-122
+ https://github.com/katela2006/Alexey
+ https://github.com/W1zard70r/Miniature-chainsaw
+ https://github.com/Mavaro1/Matvey-Skachkauskas-122b
+ https://github.com/ennseg/-122
+ https://github.com/LostnightRX/repositorii
+ https://github.com/ltlstv/sverchok_ltlstv_420/issues/1
+ https://github.com/afursovaa/itworks
+ https://github.com/kirusha02301/kirusha02301
+ https://github.com/Galua122/works
+ https://github.com/Shhamann/11
+ https://github.com/viktorblaming/dungeon
+ https://github.com/ilyechubanu/itworks
+    '''
+    t=t1.split()
+    # print(t)
+    for i in t:
+        print(f'''var http = "{i}";
+window.open(http);
+    ''')
+# g122a()
+g122b()
